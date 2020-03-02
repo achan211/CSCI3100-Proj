c665b13c890059d55f65be4561a0cecfab13d103
Justin So <justinsochi@gmail.com>
2020-03-02 22:27:58 +0800

    added new page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
