e35ebc712f372d45481bfe043d346af3d5a25aa8
1155108198 <60653012+1155108198@users.noreply.github.com>
2020-03-22 04:30:24 +0800

    Update Signup.js

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -15,8 +15,8 @@ import Container from '@material-ui/core/Container';
 
 function Copyright() {
   return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
+     <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
       <Link color="inherit" href="https://material-ui.com/">
         CUHK Live Classroom
       </Link>{' '}
