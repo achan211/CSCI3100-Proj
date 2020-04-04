d4d511c357c6c9a79f9a17bc5997c7a7ddf0422a
achan211 <60079963+achan211@users.noreply.github.com>
2020-04-04 11:10:48 +0800

    Update CoursePageButton.js

--- frontend/src/Component/CoursePageButton.js
+++ frontend/src/Component/CoursePageButton.js
@@ -1,20 +1,42 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
 
 let CoursePageButton = (props) => {
     console.log(props)
     return (
         <div>
             <div><h1>Course ID: {props.id}</h1></div>
-            <h3>Please do the styling thanks.</h3>
-            <h3>Requirements: I) 4 buttons as below.</h3>
-            <Link className="link" to={`/${props.id}/CoursePage`}><button>CoursePage(Newest Update)</button></Link>
-            
-            <Link className="link" to={`/${props.id}/Attendance`}><button>Take Attendance</button></Link>
-            <Link className="link" to={`/${props.id}/Chatroom`}><button>Chat Room</button></Link>
-            <Link className="link" to={`/${props.id}/ForumHome`}><button>Forum</button></Link>
-            <Link className="link" to={`/${props.id}/Quiz`}><button>Quiz</button></Link>
-
+            <h3>Please do the styling on CoursePageButton.js thanks.</h3>
+            {/* Button Group */}
+            <ButtonGroup orientation="vertical" aria-label="vertical outlined primary button group">
+                <Button>
+                    <Link className="link" to={`/${props.id}/CoursePage`} style={{ textDecoration: 'none' }}>
+                    CoursePage(Newest Update)
+                    </Link>
+                </Button>
+                <Button>
+                    <Link className="link" to={`/${props.id}/Attendance`} style={{ textDecoration: 'none' }}>
+                        Take Attendance
+                    </Link>
+                </Button>
+                <Button>
+                    <Link className="link" to={`/${props.id}/Chatroom`} style={{ textDecoration: 'none' }}>
+                        Class Chat Room
+                    </Link>
+                </Button>
+                <Button>
+                    <Link className="link" to={`/${props.id}/ForumHome`} style={{ textDecoration: 'none' }}>
+                        Class Forum
+                    </Link>
+                </Button>
+                <Button>
+                    <Link className="link" to={`/${props.id}/Quiz`} style={{ textDecoration: 'none' }}>
+                        Online Quiz
+                    </Link>
+                </Button>
+            </ButtonGroup>
         </div>
     )
 
