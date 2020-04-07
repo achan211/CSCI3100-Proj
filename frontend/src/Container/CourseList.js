205d786eef5fc21f4a50a77a45b272a807b7f082
achan211 <60079963+achan211@users.noreply.github.com>
2020-04-07 16:08:45 +0800

    Update CourseList.js

--- frontend/src/Container/CourseList.js
+++ frontend/src/Container/CourseList.js
@@ -1,33 +1,51 @@
-import React from 'react';
-import { Link} from "react-router-dom";
+import React from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid, Paper, List, ListItem } from "@material-ui/core";
+import Dialog from "../Component/Dialog";
+import Chat from "../Component/chat";
+import Header from "../Component/Header"
 
-
-let CourseList = (props) => {
-   
-   let renderCourseList = ()=>{ return(
-        <React.Fragment>
-            <h1> Here is the Course List page </h1>
-            <h3>Please do the styling thanks.</h3>
-            <h3>Requirements: I) N buttons as below, directing to the course landing page</h3>
-
-            <Link className="link" to="/CSCI3100"><button>CSCI3100</button></Link>
-            <Link className="link" to="/CSCI3130"><button>CSCI3130</button></Link>
-            <Link className="link" to="/CSCI3120"><button>CSCI3120</button></Link>
-            <Link className="link" to="/CSCI3110"><button>CSCI3110</button></Link>
-
-        </React.Fragment>
-    )
-   }
-   let RedirectToLogin = () =>{
-    alert("You have not yet login!");
-    const { history } = props;
-    history.push('/login');
+const useStyle = makeStyles({
+  paper: {
+    marginLeft: '50px',
+    marginRight: '50px',
   }
-    return (
-        <React.Fragment>
-        {localStorage.getItem( 'token' ) ? renderCourseList(): RedirectToLogin()}
-        </React.Fragment>
-    )
-}
+})
+let Chatroom = (props) => {
+  const classes = useStyle()
+  return (
+    <React.Fragment>
+      <div className="generalGridContainer">
+        {/* <CoursePageButton id={props.match.params.id} /> */}
+        <Header />
+        <div>
+          <Paper className={classes.paper}>
+            <div><h1>Course ID:{props.match.params.id}</h1></div>
+            <div style={{ textAlign: 'left' }}><h1 style={{ color: 'blue' }}>Messenger</h1></div>
+            <Grid container>
+              <Grid item xs={5}>
+                <List>
+                  <Dialog message="Hello" name="Alice" />
+                  <Dialog message="Hello" name="Bob" />
+                  <Dialog message="Hello" name="Charlie" />
+                </List>
+              </Grid>
+              <Grid item xs={7}>
+                <List>
+                  <Chat message="hello" name="Bob" />
+                  <Chat message="Hi" name="Bob" />
+                  <Chat message="Hi you2" name="Alice" />
+                  <Chat message="8" name="Alice" />
 
-export default CourseList
+                </List>
+              </Grid>
+            </Grid>
+          </Paper>
+        </div>
+      </div>
+    </React.Fragment>
+
+
+  );
+}
+export default Chatroom
