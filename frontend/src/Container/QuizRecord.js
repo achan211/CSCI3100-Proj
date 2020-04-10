a1a034474e43275181105d407a0f6c47ee08b380
samael-leung <60600081+samael-leung@users.noreply.github.com>
2020-04-11 02:39:08 +0800

    add graph of rechart

--- frontend/src/Container/QuizRecord.js
+++ frontend/src/Container/QuizRecord.js
@@ -11,12 +11,39 @@ import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { Link } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
-
+import { AreaChart, Area, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, ReferenceLine } from 'recharts';
+//sample data
+const data = [
+    {
+        mark: '0-20', percentage: 0.15,
+    },
+    {
+        mark: '20-30', percentage: 2.5,
+    },
+    {
+        mark: '30-40', percentage: 13.5,
+    },
+    {
+        mark: '40-50', percentage: 29,
+    },
+    {
+        mark: '50-60', percentage: 39,
+    },
+    {
+        mark: '60-70', percentage: 13.5,
+    },
+    {
+        mark: '70-80', percentage: 2.5,
+    },
+    {
+        mark: '80-100', percentage: 0.15,
+    },
+];
 
 const useStyles = makeStyles(theme => ({
     app: {
-
         flex: 1,
         display: 'flex',
         flexDirection: 'column',
@@ -31,7 +58,7 @@ const useStyles = makeStyles(theme => ({
     },
     button: {
         margin: theme.spacing(1),
-      },
+    },
 }));
 
 
@@ -42,36 +69,45 @@ let Quiz_Record = (props) => {
         return (
             <React.Fragment>
                 <div className={classes.app}>
-                    <div className={classes.main}>                     
-                            <Typography variant="h4" align="center" noWrap>
-                                <Typography variant="h4" align="left" noWrap>
-                                    <Grid item xs={12}>Select Course</Grid>
-                                </Typography>
-                                <Grid container>
-                                    <Grid item xs={4}>
-                                        <Grid container>
-                                            <List>
-                                                <ListItem button>
-                                                    <ListItemText primary = "CSCI3100" />
-                                                </ListItem>
-                                                <ListItem button>
-                                                    <ListItemText primary = "CSCI4430" />
-                                                </ListItem>
-                                                <ListItem button>
-                                                    <ListItemText primary = "ENGG1120" />
-                                                </ListItem>
-                                            </List>
-                                        </Grid>
-                                    </Grid>
-                                    <Grid item xs={8}>                                          
-                                            <Button className={classes.button} variant="outlined" color = "primary" startIcon={<ChevronLeftIcon /> } >Previous Quiz</Button>
-                                            <Button className={classes.button} variant="outlined" color = "primary" startIcon={<HomeIcon />} onClick={()=>{window.location.href="/home"}} >Back to Home page</Button>
-                                            <Button className={classes.button} variant="outlined" color = "primary" endIcon={<ChevronRightIcon />} >Next Quiz</Button><br />
-                                         
+                    <div className={classes.main}>
+                        
+                            <Typography variant="h4" align="left" noWrap>
+                                <Grid item xs={12}>Select Course</Grid>
+                            </Typography>
+                            <Grid container>
+                                <Grid item xs={2}>
+                                    <Grid container>
+                                        <List>
+                                            <ListItem button component={Link} to="/Quiz">
+                                                <ListItemText primary="CSCI3100" />
+                                            </ListItem>
+                                            <ListItem button button component={Link} to="/Quiz">
+                                                <ListItemText primary="CSCI4430" />
+                                            </ListItem >
+                                            <ListItem button button component={Link} to="/Quiz">
+                                                <ListItemText primary="ENGG1120" />
+                                            </ListItem>
+                                        </List>
                                     </Grid>
                                 </Grid>
-                            </Typography>
+                                <Grid item xs={10}>
+                                    <ResponsiveContainer width="80%" height="80%">
+                                        <AreaChart data={data} >
+                                            <CartesianGrid strokeDasharray="3 3" />
+                                            <XAxis dataKey="mark" label={{ value: 'Mark',  position: 'insideBottom' }}/>
+                                            <YAxis label={{ value: 'Percentage', angle: -90, position: 'insideLeft' }} />
+                                            <Tooltip />
+                                            <ReferenceLine x="60-70" stroke="red" label="You" />
+                                            <Area dataKey="percentage" fill="#8884d8" />
+                                        </AreaChart>
+                                    </ResponsiveContainer>
+                                    <Button className={classes.button} variant="outlined" color="primary" startIcon={<ChevronLeftIcon />} >Previous Quiz</Button>
+                                    <Button className={classes.button} variant="outlined" color="primary" startIcon={<HomeIcon />} onClick={() => { window.location.href = "/home" }} >Back to Home page</Button>
+                                    <Button className={classes.button} variant="outlined" color="primary" endIcon={<ChevronRightIcon />} >Next Quiz</Button><br />
+                                </Grid>
+                            </Grid>
                         
+
                     </div>
                 </div>
             </React.Fragment>
