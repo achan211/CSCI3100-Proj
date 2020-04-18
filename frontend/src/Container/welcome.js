a32414628dce93a39964907cdb64ff490ff557cd
Alvin Chan <Alvin@Alvin-Chans-MacBook-Pro.local>
2020-04-18 13:53:52 +0800

    Welcome Page is completed. Though I need someone to write something over the description

--- frontend/src/Container/welcome.js
+++ frontend/src/Container/welcome.js
@@ -1,129 +1,84 @@
 import React from 'react';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-import Grid from '@material-ui/core/Grid'
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography'
+import CssBaseline from '@material-ui/core/CssBaseline'
 
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-const tutorialSteps = [
-    {
-      label: 'Class Forum',
-      imgPath:
-        'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-      label: 'Attendance System',
-      imgPath:
-        'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-      label: 'Online Quiz',
-      imgPath:
-        'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-    },
-    {
-      label: 'Analysis of Scores & Attendance Rate',
-      imgPath:
-        'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-    {
-      label: 'Online Chatroom',
-      imgPath:
-        'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-    },
-  ];
 
   const useStyles = makeStyles((theme) => ({
-    slider: {
-        margin: "auto",
-        maxWidth: 400,
-        justifyContent: 'center',
+    root: {
+      marginTop: theme.spacing(5),
     },
-    header: {
-      padding: theme.spacing(2),
+
+    mainIntroText: {
+      fontSize: 22,
     },
-    img: {
-      height: 255,
-      display: 'block',
-      maxWidth: 400,
-      overflow: 'hidden',
-      width: '100%',
+
+    welcomePhoto: {
+      width: "100%"
     },
+
+    text: {
+      fontSize: 20,
+    },
+
+    title: {
+      fontSize: 24,
+    },
+
+    inGridText: {
+      margin: "auto"
+    }
   }));
   
   function Welcome() {
     const classes = useStyles();
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const maxSteps = tutorialSteps.length;
-  
-    const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
-  
-    const handleBack = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep - 1);
-    };
-  
-    const handleStepChange = (step) => {
-      setActiveStep(step);
-    };
   
     return (
         <React.Fragment>
-            <Typography variant="h1">This is a Welcome Page</Typography>
-            <Typography variant="h3">Please just ignore the Styling for now...</Typography>
-            <Grid container spacing={3}>
-                <Grid item xs={12}>
-                <div className={classes.slider}>
-                <Paper elevation={3}>
-                <Typography variant="h6" noWrap className={classes.header}>{tutorialSteps[activeStep].label}</Typography>
-                
-                <AutoPlaySwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-                >
-                {tutorialSteps.map((step, index) => (
-                    <div key={step.label}>
-                    {Math.abs(activeStep - index) <= 1 ? (
-                        <img className={classes.img} src={step.imgPath} alt={step.label} />
-                    ) : null}
-                    </div>
-                ))}
-                </AutoPlaySwipeableViews>
-                <MobileStepper
-                steps={maxSteps}
-                position="static"
-                variant="text"
-                activeStep={activeStep}
-                nextButton={
-                    <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-                    Next
-                    {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-                    </Button>
-                }
-                backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                    {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-                    Back
-                    </Button>
-                }
-                />
-                </Paper>
-                </div>
-                </Grid>
+            <CssBaseline />
+            <Grid container spacing={3} className={classes.root}>
+              <Grid item xs={12}><Typography variant="h3">Manage teaching and learning with CUHK Live Classroom</Typography></Grid>
+              <Grid item xs={12}><Typography className={classes.mainIntroText} gutterBottom>
+                CUHK Live Classroom helps students and teachers organize assignments, boost collaboration, <br />
+                and foster better communication.
+              </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}><img src="https://images.unsplash.com/photo-1524178232363-1fb2b075b655?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
+              className={classes.welcomePhoto} /></Grid>
+              <Grid item xs={12} sm={6} className={classes.inGridText}>
+                <Typography variant="h5" gutterBottom className={classes.title}>Make teaching more productive, collaborative, and meaningful</Typography>
+                <Typography variant="body1" gutterBottom color="textSecondary" className={classes.text}>CUHK Live Classroom combines Blackboard, Piazza, and all other educational software into one.
+                With CUHK Live Classroom, educators can distribute assignments, grade and send feedback, and see everything in one place.</Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom>All-in-One Course Page</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  With the simple set-up provided by CUHK Live Classroom, each course can have a fully-built course page, installed with Updates, Lecture Notes, and Homework Distribution. 
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom>Forum System</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  With the Forum System, students can ask, answer, and explore 24/7, under the guidance of their instructors
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom>Online Quiz System</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  With the Online Quiz System, just write something la...
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Typography variant="h6" gutterBottom>Attendance System</Typography>
+                <Typography variant="body1" color="textSecondary">
+                  With the Attendance System, instructor can immediately know how many students attended the class. In addition, with the analysis of attendance and quiz score, 
+                  students and instructors can monitor the learning progress. 
+                </Typography>
+              </Grid>
             </Grid>
-            
       </React.Fragment>
+      
     );
   }
   
